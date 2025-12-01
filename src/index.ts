a57c8b6df9990e9e71c2160eb3e2375977d99b13
Varunyadavgithub <yadav.varun056038@gmail.com>
2025-12-01 15:07:05 +0530

    Build a express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello World...!!!")
+// 1. Import the express
+import express, { Express, Request, Response } from "express";
+import {config} from "dotenv";
+import cors from "cors";
+
+// 2. Initialize the express
+const app: Express = express();
+config();
+
+// 3. Define a port
+const port = process.env.PORT || 8080;
+
+// Middlewares
+app.use(cors({
+    origin:process.env.HOST_URL || "*"
+}))
+
+// Basic Endpoint
+app.get("/",(req:Request,res:Response)=>{
+    res.send("Server is Live...!!!");
+})
+
+// 4. Listen a port
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
