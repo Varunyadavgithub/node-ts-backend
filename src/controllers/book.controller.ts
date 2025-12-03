1c0b7fd2f94a82056887f2617c7e99fc2c05dcef
Varunyadavgithub <yadav.varun056038@gmail.com>
2025-12-03 16:23:52 +0530

    Implement the functionality to update the book by their id

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -15,13 +15,11 @@ export const getBooks = async (req: Request, res: Response) => {
         .status(404)
         .json({ success: false, message: "No Books Found." } as IResponse);
     }
-    return res
-      .status(200)
-      .json({
-        success: true,
-        message: "Successfully fetched all Books.",
-        data: books,
-      } as IResponse);
+    return res.status(200).json({
+      success: true,
+      message: "Successfully fetched all Books.",
+      data: books,
+    } as IResponse);
   } catch (error: any) {
     return res
       .status(500)
@@ -39,6 +37,39 @@ export const addBook = async (req: Request, res: Response) => {
       publishYear,
     });
 
+    return res.status(201).json({
+      success: true,
+      message: "Book added successfully.",
+      data: book,
+    } as IResponse);
+  } catch (error: any) {
+    return res
+      .status(500)
+      .json({ success: false, message: error.message } as IResponse);
+  }
+};
+
+export const updateBook = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, author, publishYear, description } = req.body;
+
+  try {
+    const book = await Book.findByIdAndUpdate(
+      id,
+      {
+        name,
+        author,
+        publishYear,
+        description,
+      },
+      { new: true }
+    );
+
+    if (!book) {
+      return res
+        .status(404)
+        .json({ success: false, message: "No Book Found." } as IResponse);
+    }
     return res.status(201).json({
       success: true,
       message: "Book added successfully.",
--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addBook, getBooks } from "../controllers/book.controller";
+import { addBook, getBooks, updateBook } from "../controllers/book.controller";
 
 const bookRouter = Router();
 
 bookRouter.get("/get-books", getBooks);
 bookRouter.post("/add-book", addBook);
+bookRouter.put("/update-book/:id", updateBook);
 
 export default bookRouter;
