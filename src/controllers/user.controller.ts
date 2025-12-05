80c3d6208783fbf34ff0c0a91193c4f2a2b40c58
Varunyadavgithub <yadav.varun056038@gmail.com>
2025-12-05 10:23:01 +0530

    Implement the functionality to login the user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { IUser, User } from "../models/Users";
 import { IResponse } from "./book.controller";
 import bcrypt from "bcrypt";
+import jwt, { Secret } from "jsonwebtoken";
 
 export const signup = async (req: Request, res: Response) => {
   const { name, email, phone, username, password, role } = req.body;
@@ -43,6 +44,63 @@ export const signup = async (req: Request, res: Response) => {
       message: "Signup successfull.",
       data: user,
     } as IResponse);
+  } catch (error: any) {
+    return res
+      .status(500)
+      .json({ success: false, message: error.message } as IResponse);
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter a valid email.",
+        data: null,
+      } as IResponse);
+    }
+
+    let user: IUser | null;
+
+    user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Please signup.",
+        data: null,
+      } as IResponse);
+    }
+
+    // Compare the user password with database
+    let comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password.",
+        data: null,
+      } as IResponse);
+    }
+
+    const payload = {
+      id: user._id,
+      role: user.role,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECERET as Secret, {
+      expiresIn: "1hr",
+    });
+
+    return res
+      .status(200)
+      .cookie("token", token)
+      .json({
+        success: true,
+        message: "Login successfull.",
+        data: user,
+      } as IResponse);
   } catch (error: any) {
     return res
       .status(500)
--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,6 +1,7 @@
 import mongoose, { model, mongo, Schema } from "mongoose";
 
 interface IUser {
+  _id?: string;
   name: string;
   email: string;
   phone: string;
